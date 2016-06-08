08ada6054f1e71c85da66245c0ef209474128ff8
J Barrasa <jbarrasa@Js-MacBook-Pro.local>
2016-06-09 00:04:24 +0100

    Added shortening or URIs in element names and parametrizable periodic commit

--- src/main/java/semantics/RDFImport.java
+++ src/main/java/semantics/RDFImport.java
@@ -1,9 +1,8 @@
 package semantics;
 
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.QueryExecutionException;
-import org.neo4j.graphdb.Result;
-import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.*;
+import org.neo4j.graphdb.schema.IndexDefinition;
+import org.neo4j.graphdb.schema.Schema;
 import org.neo4j.kernel.internal.GraphDatabaseAPI;
 import org.neo4j.logging.Log;
 import org.neo4j.procedure.Context;
@@ -11,6 +10,7 @@ import org.neo4j.procedure.Name;
 import org.neo4j.procedure.PerformsWrites;
 import org.neo4j.procedure.Procedure;
 import org.openrdf.model.*;
+import org.openrdf.model.Resource;
 import org.openrdf.model.vocabulary.RDF;
 import org.openrdf.model.vocabulary.XMLSchema;
 import org.openrdf.rio.*;
@@ -20,21 +20,17 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.Callable;
 import java.util.stream.Stream;
 
 /**
  * Created by jbarrasa on 21/03/2016.
- *
+ * <p>
  * RDF importer based on:
  * 1. DatatypeProperties become node attributes
  * 2. rdf:type relationships are transformed into labels on the subject node
  * 3. rdf:type relationships generate :Class nodes on the object
- *
  */
 public class RDFImport {
     @Context
@@ -48,12 +44,14 @@ public class RDFImport {
 
     @Procedure
     @PerformsWrites
-    public Stream<ImportResults> importRDF(@Name("url") String url, @Name("format") String format) {
+    public Stream<ImportResults> importRDF(@Name("url") String url, @Name("format") String format,
+                                           @Name("shorten") boolean shortenUrls, @Name("commitSize") long commitSize) {
         ImportResults importResults = new ImportResults();
         URL documentUrl;
-
-        StatementLoader statementLoader = new StatementLoader(db, 25000);
+        // hardcoded periodic commit and shortening
+        StatementLoader statementLoader = new StatementLoader(db, (commitSize > 0 ? commitSize : 5000), shortenUrls);
         try {
+            checkIndexesExist();
             documentUrl = new URL(url);
             InputStream inputStream = documentUrl.openStream();
             RDFParser rdfParser = Rio.createParser(getFormat(format));
@@ -61,12 +59,33 @@ public class RDFImport {
             rdfParser.parse(inputStream, documentUrl.toString());
         } catch (MalformedURLException e) {
             e.printStackTrace();
-        } catch (IOException | RDFHandlerException | QueryExecutionException | RDFParseException e) {
+        } catch (IOException | RDFHandlerException | QueryExecutionException | RDFParseException | RDFImportPreRequisitesNotMet e) {
             importResults.setTerminationKO(e.getMessage());
         } finally {
             importResults.setTriplesLoaded(statementLoader.getIngestedTriples());
+            importResults.setNamespaces(statementLoader.getNamespaces());
         }
         return Stream.of(importResults);
+    }
+
+    private void checkIndexesExist() throws RDFImportPreRequisitesNotMet {
+        Iterable<IndexDefinition> indexes = db.schema().getIndexes();
+        if(missing(indexes.iterator(),"Resource")||missing(indexes.iterator(),"URI")||
+                missing(indexes.iterator(),"BNode")||missing(indexes.iterator(),"Class")){
+            throw new RDFImportPreRequisitesNotMet("At least one of the required indexes was not found " +
+                    "[ :Resource(uri), :URI(uri), :BNode(uri), :Class(uri) ]");
+        }
+    }
+
+    private boolean missing(Iterator<IndexDefinition> iterator, String indexLabel) {
+        while (iterator.hasNext()){
+            IndexDefinition indexDef = iterator.next();
+            if(indexDef.getLabel().name().equals(indexLabel) &&
+                    indexDef.getPropertyKeys().iterator().next().equals("uri")) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private RDFFormat getFormat(String format) {
@@ -79,62 +98,119 @@ public class RDFImport {
         return RDFFormat.TURTLE; //some default
     }
 
-    class StatementLoader extends RDFHandlerBase implements Callable<Integer>{
+    class StatementLoader extends RDFHandlerBase implements Callable<Integer> {
 
+        private final boolean shortenUris;
         private int ingestedTriples = 0;
         private GraphDatabaseService graphdb;
-        private int commitFreq;
+        private long commitFreq;
         private List<QueryWithParams> cypherStatements = new ArrayList<QueryWithParams>();
-        //private List<Map<String, Object>> cypherStatementParams = new ArrayList<Map<String, Object>>();
+        private Map<String,String> namespaces =  new HashMap<>();
 
-        public StatementLoader(GraphDatabaseService db, int batchSize) {
+        public StatementLoader(GraphDatabaseService db, long batchSize, boolean shortenUrls) {
             graphdb = db;
             commitFreq = batchSize;
+            shortenUris = shortenUrls;
+        }
+
+        @Override
+        public void startRDF() throws RDFHandlerException {
+            getExistingNamespaces();
+            log.info("Found " + namespaces.size() + " namespaces in the DB: " + namespaces);
+        }
+
+        private void getExistingNamespaces() {
+            Result nslist = graphdb.execute("MATCH (n:NamespacePrefixDefinition) \n" +
+                    "UNWIND keys(n) AS namespace\n" +
+                    "RETURN namespace, n[namespace] as prefix");
+            while (nslist.hasNext()){
+                Map<String, Object> ns = nslist.next();
+                namespaces.put((String)ns.get("namespace"),(String)ns.get("prefix"));
+            }
         }
 
         @Override
         public void endRDF() throws RDFHandlerException {
-            int finalCount = Utils.inTx(graphdb,this);
-            ingestedTriples+= finalCount;
-            log.info("Successfully committed " + finalCount + " cypher statements. Total number of triples imported is " + ingestedTriples);
+            int finalCount = Utils.inTx(graphdb, this);
+            ingestedTriples += finalCount;
+            addNamespaceNode();
+
+            log.info("Successfully committed " + finalCount + " cypher statements. " +
+                    "Total number of triples imported is " + ingestedTriples);
+        }
+
+        private void addNamespaceNode() {
+            Map<String, Object> params = new HashMap<>();
+            params.put("props", namespaces);
+            graphdb.execute("MERGE (n:NamespacePrefixDefinition) SET n+={props}", params);
         }
 
         @Override
         public void handleStatement(Statement st) {
-            URI predicate = st.getPredicate();
+            IRI predicate = st.getPredicate();
             Resource subject = st.getSubject(); //includes blank nodes
             Value object = st.getObject();
             String cypher;
             if (object instanceof Literal) {
                 // known issue: does not deal with multivalued properties. Probably the obvious approach would be
                 // to create arrays when multiple values
+                // predicates are always URIs
                 Map<String, Object> params = new HashMap<>();
                 params.put("subject", subject.stringValue().replace("'", "\'"));
                 params.put("object", getObjectValue((Literal) object));
-                cypherStatements.add(new QueryWithParams(String.format("MERGE (x:Resource:%s {uri:{subject}}) SET x.`%s` = {object}",
-                        (subject instanceof BNode ? "BNode" : "URI"), predicate.stringValue()), params));
-            } else if (predicate.equals(RDF.TYPE) && !(object instanceof BNode)){
+                cypherStatements.add(new QueryWithParams(String.format("MERGE (x:Resource:%s {uri:{subject}}) " +
+                        "SET x.`%s` = {object}", (subject instanceof BNode ? "BNode" : "URI"), shorten(predicate)),
+                        params));
+            } else if (predicate.equals(RDF.TYPE) && !(object instanceof BNode)) {
                 // Optimization -> rdf:type is transformed into a label. Reduces node density and uses indexes.
                 Map<String, Object> params = new HashMap<>();
                 params.put("subject", subject.stringValue().replace("'", "\'"));
-                params.put("object", object.stringValue());
-                cypherStatements.add(new QueryWithParams(String.format("MERGE (x:Resource:%s {uri:{subject}}) SET x:`%s` MERGE (y:%s {uri:{object}}) SET y:Class",
+                params.put("object", (object instanceof BNode ? object.stringValue() : shorten((IRI)object)));
+                cypherStatements.add(new QueryWithParams(String.format("MERGE (x:Resource:%s {uri:{subject}}) " +
+                                "SET x:`%s` MERGE (y:%s {uri:{object}}) SET y:Class",
                         (subject instanceof BNode ? "BNode" : "URI"),
-                        object.stringValue(), (object instanceof BNode ? "BNode" : "URI")), params));
+                        (object instanceof BNode ? object.stringValue() : shorten((IRI)object)),
+                        (object instanceof BNode ? "BNode" : "URI")), params));
             } else {
                 Map<String, Object> params = new HashMap<>();
                 params.put("subject", subject.stringValue().replace("'", "\'"));
                 params.put("object", object.stringValue());
-                cypherStatements.add(new QueryWithParams(String.format("MERGE (x:Resource:%s {uri:{subject}}) MERGE (y:Resource:%s {uri:{object}}) MERGE (x)-[:`%s`]->(y)",
+                cypherStatements.add(new QueryWithParams(String.format("MERGE (x:Resource:%s {uri:{subject}}) " +
+                                "MERGE (y:Resource:%s {uri:{object}}) MERGE (x)-[:`%s`]->(y)",
                         (subject instanceof BNode ? "BNode" : "URI"),
                         (object instanceof BNode ? "BNode" : "URI"),
-                        predicate.stringValue()),params));
+                        shorten(predicate)), params));
             }
 
-            if (cypherStatements.size()%commitFreq == 0) {
-                ingestedTriples+= Utils.inTx(graphdb,this);
-                log.info("Successful periodic commit of " + commitFreq + " cypher statements. " + ingestedTriples + " triples ingested so far...");
+            if (cypherStatements.size() % commitFreq == 0) {
+                ingestedTriples += Utils.inTx(graphdb, this);
+                log.info("Successful periodic commit of " + commitFreq + " cypher statements. " +
+                        ingestedTriples + " triples ingested so far...");
             }
+        }
+
+        private String shorten(IRI iri) {
+            if (shortenUris) {
+                String localName = iri.getLocalName();
+                String prefix = getPrefix(iri.getNamespace());
+                return prefix + "_" + localName;
+            } else
+            {
+                return iri.stringValue();
+            }
+        }
+
+        private String getPrefix(String namespace) {
+            if (namespaces.containsKey(namespace)){
+                return namespaces.get(namespace);
+            } else{
+                namespaces.put(namespace, createPrefix(namespace));
+                return namespaces.get(namespace);
+            }
+        }
+
+        private String createPrefix(String namespace) {
+            return "ns" + namespaces.size();
         }
 
         private String getObjectValue(Literal object) {
@@ -154,11 +230,16 @@ public class RDFImport {
         public int getIngestedTriples() {
             return ingestedTriples;
         }
+        public Map<String,String> getNamespaces() {
+
+            return namespaces;
+        }
+
 
         @Override
         public Integer call() throws Exception {
             int count = 0;
-            for(QueryWithParams qwp : cypherStatements){
+            for (QueryWithParams qwp : cypherStatements) {
                 graphdb.execute(qwp.getCypherString(), qwp.getParams());
                 count++;
             }
@@ -191,10 +272,15 @@ public class RDFImport {
     public static class ImportResults {
         public String terminationStatus = "OK";
         public long triplesLoaded = 0;
+        public Map<String,String> namespaces;
         public String extraInfo = "";
 
         public void setTriplesLoaded(long triplesLoaded) {
             this.triplesLoaded = triplesLoaded;
+        }
+
+        public void setNamespaces(Map<String, String> namespaces) {
+            this.namespaces = namespaces;
         }
 
         public void setTerminationKO(String message) {
@@ -204,4 +290,16 @@ public class RDFImport {
 
     }
 
+    private class RDFImportPreRequisitesNotMet extends Exception {
+        String message;
+
+        public RDFImportPreRequisitesNotMet(String s) {
+            message = s;
+        }
+
+        @Override
+        public String getMessage() {
+            return message;
+        }
+    }
 }
--- src/test/java/semantics/RDFImportTest.java
+++ src/test/java/semantics/RDFImportTest.java
@@ -7,6 +7,9 @@ import org.neo4j.kernel.impl.proc.Procedures;
 import org.neo4j.kernel.internal.GraphDatabaseAPI;
 import org.neo4j.test.TestGraphDatabaseFactory;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -17,12 +20,30 @@ import static org.junit.Assert.assertTrue;
 public class RDFImportTest {
 
     @Test
-    public void testImportJSONLD() throws Exception {
+    public void testAbortIfNoIndices() throws Exception {
         GraphDatabaseService db = new TestGraphDatabaseFactory().newImpermanentDatabase();
         ((GraphDatabaseAPI)db).getDependencyResolver().resolveDependency(Procedures.class).register(RDFImport.class);
 
         Result importResults1 = db.execute("CALL semantics.importRDF('" +
-                RDFImportTest.class.getClassLoader().getResource("mini-ld.json").toURI() + "','JSON-LD')");
+                RDFImportTest.class.getClassLoader().getResource("mini-ld.json").toURI() + "','JSON-LD',false,500)");
+
+        Map<String, Object> singleResult = importResults1.next();
+
+        assertEquals(new Long(0), singleResult.get("triplesLoaded"));
+        assertEquals("KO", singleResult.get("terminationStatus"));
+        assertEquals("At least one of the required indexes was not found [ :Resource(uri), :URI(uri), :BNode(uri), :Class(uri) ]", singleResult.get("extraInfo"));
+
+    }
+
+    @Test
+    public void testImportJSONLD() throws Exception {
+        GraphDatabaseService db = new TestGraphDatabaseFactory().newImpermanentDatabase();
+        ((GraphDatabaseAPI)db).getDependencyResolver().resolveDependency(Procedures.class).register(RDFImport.class);
+
+        createIndices(db);
+
+        Result importResults1 = db.execute("CALL semantics.importRDF('" +
+                RDFImportTest.class.getClassLoader().getResource("mini-ld.json").toURI() + "','JSON-LD',false,500)");
         assertEquals(new Long(6), importResults1.next().get("triplesLoaded"));
         assertEquals("http://me.markus-lanthaler.com/",
                 db.execute("MATCH (n{`http://xmlns.com/foaf/0.1/name` : 'Markus Lanthaler'}) RETURN n.uri AS uri")
@@ -33,13 +54,40 @@ public class RDFImportTest {
     }
 
     @Test
+    public void testImportJSONLDShortening() throws Exception {
+        GraphDatabaseService db = new TestGraphDatabaseFactory().newImpermanentDatabase();
+        ((GraphDatabaseAPI)db).getDependencyResolver().resolveDependency(Procedures.class).register(RDFImport.class);
+
+        createIndices(db);
+
+        Result importResults1 = db.execute("CALL semantics.importRDF('" +
+                RDFImportTest.class.getClassLoader().getResource("mini-ld.json").toURI() + "','JSON-LD',true,500)");
+        assertEquals(new Long(6), importResults1.next().get("triplesLoaded"));
+        assertEquals("http://me.markus-lanthaler.com/",
+                db.execute("MATCH (n{ns0_name : 'Markus Lanthaler'}) RETURN n.uri AS uri")
+                        .next().get("uri"));
+        assertEquals(new Long(1),
+                db.execute("MATCH (n) WHERE exists(n.ns0_modified) RETURN count(n) AS count")
+                        .next().get("count"));
+
+        HashMap<String, String> expectedNamespaceDefs = new HashMap<String, String>();
+        expectedNamespaceDefs.put("baseName", "http://xmlns.com/foaf/0.1/");
+        expectedNamespaceDefs.put("prefix", "ns0");
+        assertEquals(expectedNamespaceDefs,
+                db.execute("MATCH (n:NamespacePrefixDefinition) RETURN { baseName: keys(n)[0] ,  prefix: n[keys(n)[0]]} AS namespaces")
+                        .next().get("namespaces"));
+    }
+
+    @Test
     public void testImportRDFXML() throws Exception {
         GraphDatabaseService db = new TestGraphDatabaseFactory().newImpermanentDatabase();
         ((GraphDatabaseAPI)db).getDependencyResolver().resolveDependency(Procedures.class).register(RDFImport.class);
 
+        createIndices(db);
+
         Result importResults1 = db.execute("CALL semantics.importRDF('" +
                 RDFImportTest.class.getClassLoader().getResource("jeu-de-donnees-des-jeux-de-donnees-open-data-paris.rdf")
-                        .toURI() + "','RDF/XML')");
+                        .toURI() + "','RDF/XML',false,500)");
         assertEquals(new Long(38), importResults1.next().get("triplesLoaded"));
         assertEquals(new Long(7),
                 db.execute("MATCH ()-[r:`http://purl.org/dc/terms/relation`]->(b) RETURN count(b) as count")
@@ -51,13 +99,106 @@ public class RDFImportTest {
     }
 
     @Test
+    public void testImportRDFXMLShortening() throws Exception {
+        GraphDatabaseService db = new TestGraphDatabaseFactory().newImpermanentDatabase();
+        ((GraphDatabaseAPI)db).getDependencyResolver().resolveDependency(Procedures.class).register(RDFImport.class);
+
+        createIndices(db);
+
+        Result importResults1 = db.execute("CALL semantics.importRDF('" +
+                RDFImportTest.class.getClassLoader().getResource("jeu-de-donnees-des-jeux-de-donnees-open-data-paris.rdf")
+                        .toURI() + "','RDF/XML',true,500)");
+        assertEquals(new Long(38), importResults1.next().get("triplesLoaded"));
+        assertEquals(new Long(7),
+                db.execute("MATCH ()-[r:ns2_relation]->(b) RETURN count(b) as count")
+                        .next().get("count"));
+
+        assertEquals("http://opendata.paris.fr/opendata/jsp/site/Portal.jsp?document_id=109&portlet_id=106",
+                db.execute("MATCH (x:BNode) WHERE x.ns4_label = 'harvest_dataset_url'" +
+                        "\nRETURN x.ns5_value AS datasetUrl").next().get("datasetUrl"));
+
+        assertEquals("ns0",
+                db.execute("MATCH (n:NamespacePrefixDefinition) \n" +
+                        "RETURN n.`http://www.w3.org/ns/dcat#` as prefix")
+                        .next().get("prefix"));
+
+    }
+
+    @Test
+    public void testImportRDFXMLShorteningWithPrefixPreDefinition() throws Exception {
+        GraphDatabaseService db = new TestGraphDatabaseFactory().newImpermanentDatabase();
+        ((GraphDatabaseAPI)db).getDependencyResolver().resolveDependency(Procedures.class).register(RDFImport.class);
+
+        createIndices(db);
+
+        //prefix predefinition by creating the NamespacePrefixDefinitionNode
+        db.execute("WITH {`http://purl.org/dc/terms/`:'dc',\n" +
+                "`http://www.w3.org/1999/02/22-rdf-syntax-ns#`:'rdf',\n" +
+                "`http://www.w3.org/2002/07/owl#`:'owl',\n" +
+                "`http://www.w3.org/ns/dcat#`:'dcat',\n" +
+                "`http://www.w3.org/2000/01/rdf-schema#`:'rdfs',\n" +
+                "`http://xmlns.com/foaf/0.1/`:'foaf'} as nslist\n" +
+                "MERGE (n:NamespacePrefixDefinition)\n" +
+                "SET n+=nslist");
+
+        Result importResults1 = db.execute("CALL semantics.importRDF('" +
+                RDFImportTest.class.getClassLoader().getResource("jeu-de-donnees-des-jeux-de-donnees-open-data-paris.rdf")
+                        .toURI() + "','RDF/XML',true,500)");
+        assertEquals(new Long(38), importResults1.next().get("triplesLoaded"));
+        assertEquals(new Long(7),
+                db.execute("MATCH ()-[r:dc_relation]->(b) RETURN count(b) as count")
+                        .next().get("count"));
+
+        assertEquals("http://opendata.paris.fr/opendata/jsp/site/Portal.jsp?document_id=109&portlet_id=106",
+                db.execute("MATCH (x) WHERE x.rdfs_label = 'harvest_dataset_url'" +
+                        "\nRETURN x.rdf_value AS datasetUrl").next().get("datasetUrl"));
+
+        assertEquals("dcat",
+                db.execute("MATCH (n:NamespacePrefixDefinition) \n" +
+                        "RETURN n.`http://www.w3.org/ns/dcat#` as prefix")
+                        .next().get("prefix"));
+
+    }
+
+
+    @Test
+    public void testImportRDFXMLShorteningWithPrefixPreDefinitionOneTriple() throws Exception {
+        GraphDatabaseService db = new TestGraphDatabaseFactory().newImpermanentDatabase();
+        ((GraphDatabaseAPI)db).getDependencyResolver().resolveDependency(Procedures.class).register(RDFImport.class);
+
+        createIndices(db);
+
+        //prefix predefinition by creating the NamespacePrefixDefinitionNode
+        db.execute("WITH {`http://neo4j.com/voc/`:'voc' } as nslist\n" +
+                "MERGE (n:NamespacePrefixDefinition)\n" +
+                "SET n+=nslist " +
+                "RETURN n ");
+
+        Result importResults1 = db.execute("CALL semantics.importRDF('" +
+                RDFImportTest.class.getClassLoader().getResource("oneTriple.rdf")
+                        .toURI() + "','RDF/XML',true,500)");
+        assertEquals(new Long(1), importResults1.next().get("triplesLoaded"));
+        assertEquals("JB",
+                db.execute("MATCH (jb {uri: 'http://neo4j.com/invividual/JB'}) RETURN jb.voc_name AS name")
+                        .next().get("name"));
+
+        assertEquals("voc",
+                db.execute("MATCH (n:NamespacePrefixDefinition) \n" +
+                        "RETURN n.`http://neo4j.com/voc/` as prefix")
+                        .next().get("prefix"));
+
+    }
+
+    @Test
     public void testImportTurtle() throws Exception {
         GraphDatabaseService db = new TestGraphDatabaseFactory().newImpermanentDatabase();
         ((GraphDatabaseAPI)db).getDependencyResolver().resolveDependency(Procedures.class).register(RDFImport.class);
 
+        createIndices(db);
+
         Result importResults1 = db.execute("CALL semantics.importRDF('" +
                 RDFImportTest.class.getClassLoader().getResource("opentox-example.ttl")
-                        .toURI() + "','TURTLE')");
+                        .toURI() + "','TURTLE',false,500)");
         assertEquals(new Long(157), importResults1.next().get("triplesLoaded"));
         Result algoNames = db.execute("MATCH (n:`http://www.opentox.org/api/1.1#Algorithm`) " +
                 "\nRETURN n.`http://purl.org/dc/elements/1.1/title` AS algos ORDER By algos");
@@ -69,6 +210,13 @@ public class RDFImportTest {
         assertEquals("http://www.opentox.org/example/1.1#benzene", compounds.next().get("compound"));
         assertEquals("http://www.opentox.org/example/1.1#phenol", compounds.next().get("compound"));
 
+    }
+
+    private void createIndices(GraphDatabaseService db) {
+        db.execute("CREATE INDEX ON :Resource(uri)");
+        db.execute("CREATE INDEX ON :URI(uri)");
+        db.execute("CREATE INDEX ON :BNode(uri)");
+        db.execute("CREATE INDEX ON :Class(uri)");
     }
 
 }
