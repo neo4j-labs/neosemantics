b4fce568b1a64741b814b087da800c28cdaf41e8
J Barrasa <jbarrasa@Js-MacBook-Pro.local>
2017-02-14 17:55:15 +0000

    removed unneeded dependency. changed test to local

--- src/test/java/semantics/LiteOntologyImporterTest.java
+++ src/test/java/semantics/LiteOntologyImporterTest.java
@@ -41,17 +41,19 @@ public class LiteOntologyImporterTest {
         GraphDatabaseService db = new TestGraphDatabaseFactory().newImpermanentDatabase();
         ((GraphDatabaseAPI)db).getDependencyResolver().resolveDependency(Procedures.class).registerProcedure(LiteOntologyImporter.class);
 
-        Result importResult = db.execute("CALL semantics.liteOntoImport('http://topbraid.org/schema/schema.rdf','RDF/XML')");
+        Result importResult = db.execute("CALL semantics.liteOntoImport('" +
+                        LiteOntologyImporterTest.class.getClassLoader().getResource("schema.rdf").toURI() +
+                "','RDF/XML')");
 
 //        assertEquals(new Long(16), importResult.next().get("elementsLoaded"));
 
-        assertEquals(new Long(2), db.execute("MATCH (n:Class) RETURN count(n) AS count").next().get("count"));
+        assertEquals(new Long(592), db.execute("MATCH (n:Class) RETURN count(n) AS count").next().get("count"));
 
-        assertEquals(new Long(5), db.execute("MATCH (n:DatatypeProperty)-[:DOMAIN]->(:Class)  RETURN count(n) AS count").next().get("count"));
+        assertEquals(new Long(343), db.execute("MATCH (n:DatatypeProperty)-[:DOMAIN]->(:Class)  RETURN count(n) AS count").next().get("count"));
 
-        assertEquals(new Long(3), db.execute("MATCH (n:DatatypeProperty)-[:DOMAIN]->(:ObjectProperty) RETURN count(n) AS count").next().get("count"));
+        assertEquals(new Long(0), db.execute("MATCH (n:DatatypeProperty)-[:DOMAIN]->(:ObjectProperty) RETURN count(n) AS count").next().get("count"));
 
-        assertEquals(new Long(6), db.execute("MATCH (n:ObjectProperty) RETURN count(n) AS count").next().get("count"));
+        assertEquals(new Long(377), db.execute("MATCH (n:ObjectProperty) RETURN count(n) AS count").next().get("count"));
 
     }
 
