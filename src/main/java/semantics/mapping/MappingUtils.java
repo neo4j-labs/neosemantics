30140f957f1446d3f0450f0c9ea2b90fa3773886
Jesus Barrasa <jbarrasa@outlook.com>
2018-10-06 10:47:36 +0100

    added unit tests for mapping utils methods

--- src/main/java/semantics/mapping/MappingUtils.java
+++ src/main/java/semantics/mapping/MappingUtils.java
@@ -18,17 +18,19 @@ public class MappingUtils {
     @Context
     public Log log;
 
-    // public static RDFFormat[] availableParsers = new RDFFormat[]{RDFFormat.RDFXML, RDFFormat.JSONLD, RDFFormat.TURTLE,
-    //        RDFFormat.NTRIPLES, RDFFormat.TRIG};
-
-
     @Procedure(mode = Mode.WRITE)
     public Stream<NodeResult> addSchema(@Name("schemaUri") String uri, @Name("prefix") String prefix) {
+        //what should the logic be??? no two prefixes for the same ns and no more than one prefix for a voc
         Map<String,Object> params = new HashMap<>();
         params.put("schema",uri);
         params.put("prefix",prefix);
-        String cypher = "MERGE (mns:_MapNs { _ns: mappingDef.namespace, _prefix : mappingDef.prefix }) return mns";
-        return db.execute(cypher, params).stream().map(n -> (Node) n.get("mns")).map(NodeResult::new);
+        String checkIfSchemaOrPrefixExist = "MATCH (mns:_MapNs { _ns: $schema}) RETURN mns UNION MATCH (mns:_MapNs { _prefix : $prefix }) RETURn mns ";
+        if (db.execute(checkIfSchemaOrPrefixExist, params).hasNext()) {
+            return null;
+        } else {
+            String createNewSchema = "CREATE (mns:_MapNs { _ns: $schema, _prefix : $prefix }) return mns";
+            return db.execute(createNewSchema, params).stream().map(n -> (Node) n.get("mns")).map(NodeResult::new);
+        }
     }
 
     @Procedure(mode = Mode.WRITE)
@@ -52,17 +54,20 @@ public class MappingUtils {
                 "{ namespace: 'https://spec.edmcouncil.org/fibo/ontology/FND/Relations/Relations/', prefix: 'fibo-fnd-rel-rel' },\n" +
                 "{ namespace: 'https://spec.edmcouncil.org/fibo/ontology/FND/Utilities/AnnotationVocabulary/', prefix: 'fibo-fnd-utl-av' }\n" +
                 "] AS schemaDef \n" +
-                "MERGE (mns:_MapNs { _ns: schemaDef.namespace, _prefix : schemaDef.prefix }) RETURN mns";
+                " CALL semantics.mapping.addSchema(schemaDef.namespace, schemaDef.prefix) YIELD node AS mns" +
+                " RETURN mns";
+
         return db.execute(cypher).stream().map(n -> (Node) n.get("mns")).map(NodeResult::new);
     }
 
     @Procedure(mode = Mode.READ)
-    public Stream<NodeResult> listSchemas(@Name(value="schemaUri",defaultValue = "") String schemaUri) {
+    public Stream<NodeResult> listSchemas(@Name(value="searchString",defaultValue = "") String searchString) {
 
         Map<String,Object> params = new HashMap<>();
-        params.put("schemaUri",schemaUri);
+        params.put("searchString",searchString);
 
-        String cypher = (schemaUri.trim().equals("")?"MATCH (mns:_MapNs) RETURN mns ":"MATCH (mns:_MapNs) WHERE mns._ns CONTAINS $schemaUri RETURN mns ");
+        String cypher = (searchString.trim().equals("")?"MATCH (mns:_MapNs) RETURN mns ":
+                "MATCH (mns:_MapNs) WHERE mns._ns CONTAINS $searchString OR mns._prefix CONTAINS $searchString RETURN mns ");
 
         return db.execute(cypher, params).stream().map(n -> (Node) n.get("mns")).map(NodeResult::new);
     }
@@ -77,10 +82,44 @@ public class MappingUtils {
         params.put("localName",schElem);
 
         Node newMapping = (Node)db.execute(cypher, params).next().get("mp");
-        schema.createRelationshipTo(newMapping, RelationshipType.withName("_IN"));
+        newMapping.createRelationshipTo(schema, RelationshipType.withName("_IN"));
         return Stream.of(new NodeResult(newMapping));
     }
 
+    @Procedure(mode = Mode.WRITE)
+    public Stream<StringOutput> dropSchema(@Name("schemaUri") String schemaUri) {
+        String cypher = "MATCH (mns:_MapNs { _ns: $ns } )<-[:_IN]-(elem:_MapDef) DETACH DELETE mns, elem RETURN count(elem) as ct";
+        Map<String,Object> params = new HashMap<>();
+        params.put("ns",schemaUri);
+        return Stream.of(new StringOutput("successfully deleted schema with " + db.execute(cypher, params).next().get("ct").toString() + " mappings"));
+    }
 
 
+    @Procedure(mode = Mode.WRITE)
+    public Stream<StringOutput> dropMapping(@Name("graphElementName") String gElem) {
+        String cypher = "MATCH (elem:_MapDef { _key : $local }) DETACH DELETE elem RETURN count(elem) as ct";
+        Map<String,Object> params = new HashMap<>();
+        params.put("local",gElem);
+        return Stream.of(new StringOutput(((Integer)db.execute(cypher, params).next().get("ct")).equals(1)?"successfully deleted mapping":"mapping not found"));
+    }
+
+    @Procedure(mode = Mode.READ)
+    public Stream<NodeResult> listMappings(@Name(value="schemaUri",defaultValue = "") String schemaUri,
+                                           @Name(value="schemaElem",defaultValue = "") String schemaElem) {
+
+        Map<String,Object> params = new HashMap<>();
+        params.put("schemaUri",schemaUri);
+
+        String cypher = ("MATCH (mns:_MapNs)<-[:_IN]-(elem:_MapDef) WHERE mns._ns CONTAINS $schemaUri AND elem._key CONTAINS $elemName RETURN mns, elem ");
+        //incomplete
+        return db.execute(cypher, params).stream().map(n -> (Node) n.get("mns")).map(NodeResult::new);
+    }
+
+    public class StringOutput {
+        public String output;
+
+        public StringOutput(String output) {
+            this.output = output;
+        }
+    }
 }
