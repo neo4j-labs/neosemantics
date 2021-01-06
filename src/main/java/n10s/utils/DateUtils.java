b6856da615037a9be37439a23f4413ab912f3338
J. Barrasa <jbarrasa@users.noreply.github.com>
2021-01-06 18:27:32 +0000

    Update DateUtils.java
    added method for xsd:date

--- src/main/java/n10s/utils/DateUtils.java
+++ src/main/java/n10s/utils/DateUtils.java
@@ -2,6 +2,7 @@ package n10s.utils;
 
 import java.time.DateTimeException;
 import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 import java.util.Calendar;
 
@@ -49,6 +50,41 @@ public class DateUtils {
 			throw new IllegalArgumentException(parserErrors.toString());
 		} else {
 			return localDateTime;
+		}
+	}
+
+	public static LocalDate parseDate(String stringDate) {
+		boolean dateParsed = false;
+		LocalDate localDate = null;
+		StringBuilder parserErrors = new StringBuilder("Error parsing ").append(stringDate).append(":\n");
+
+		/* Try date parsing with LocalDateTime.parse */
+		try {
+			localDate = LocalDate.parse(stringDate);
+			dateParsed = true;
+		} catch (DateTimeParseException e) {
+			dateParsed = false;
+			parserErrors.append(e.getMessage()).append("\n");
+		}
+
+		/* If date is not parsed */
+		if (!dateParsed) {
+			/* Try with DatatypeConverter.parseDateTime */
+			try {
+				Calendar calendar = DatatypeConverter.parseDate(stringDate);
+				localDate = LocalDate.ofInstant(calendar.toInstant(), calendar.getTimeZone().toZoneId());
+				dateParsed = true;
+			} catch (IllegalArgumentException | DateTimeException e) {
+				dateParsed = false;
+				parserErrors.append(e.getMessage()).append("\n");
+			}
+		}
+
+		/* If date is not parsed, throw exception */
+		if (!dateParsed) {
+			throw new IllegalArgumentException(parserErrors.toString());
+		} else {
+			return localDate;
 		}
 	}
 }
