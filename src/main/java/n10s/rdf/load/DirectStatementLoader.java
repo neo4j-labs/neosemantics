25f95b3db0650ca87b551f58b19065ed2dd0e71c
JB <jbarrasa@outlook.com>
2022-11-11 12:37:07 +0000

    first complete version

--- src/main/java/n10s/rdf/load/DirectStatementLoader.java
+++ src/main/java/n10s/rdf/load/DirectStatementLoader.java
@@ -40,7 +40,16 @@ public class DirectStatementLoader extends RDFToLPGStatementProcessor {
 
   @Override
   public void endRDF() throws RDFHandlerException {
-    periodicOperation();
+    if(reuseTx){
+      namespaces.partialRefresh(tx);
+      log.debug("namespace prefixes synced: " + namespaces.toString());
+      this.runPartialTx(tx);
+      log.debug("rdf import commit: " + mappedTripleCounter + " triples ingested.");
+      //not sure this is needed here
+      totalTriplesMapped += mappedTripleCounter;
+    } else {
+      periodicOperation();
+    }
     log.debug("Import complete: " + totalTriplesMapped + "  triples ingested out of "
         + totalTriplesParsed + " parsed");
   }
